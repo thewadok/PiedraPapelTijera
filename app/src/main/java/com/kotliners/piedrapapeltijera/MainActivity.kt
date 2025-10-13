4391b395ff05ec4883240bd4406002235c6020a5
thewadok <erodriguezrodriguez5@uoc.edu>
2025-10-13 21:00:50 +0200

    Clase Screen con rutas y titulos de las pantallas

--- app/src/main/java/com/kotliners/piedrapapeltijera/MainActivity.kt
+++ app/src/main/java/com/kotliners/piedrapapeltijera/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.widget.DialogTitle
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kotliners.piedrapapeltijera.ui.theme.PiedraPapelTijeraTheme
+
+/*Rutas*/
+sealed class Screen(val route: String, val title: String){
+    data object Splash : Screen("splash","Splash")
+    data object Home : Screen( "home", "hinicio")
+    data object History : Screen( "ranking", "Ranking")
+    data object Setting : Screen("setting", "Ajustes")
+    data object Help : Screen("help","Ayuda")
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

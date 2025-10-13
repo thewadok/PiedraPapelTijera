ea01db376a89b89660f9cc738bbe78b169d9f361
thewadok <erodriguezrodriguez5@uoc.edu>
2025-10-13 21:50:58 +0200

    Ajustes en las dependencias

--- app/src/main/java/com/kotliners/piedrapapeltijera/MainActivity.kt
+++ app/src/main/java/com/kotliners/piedrapapeltijera/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.kotliners.piedrapapeltijera
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.widget.DialogTitle
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.kotliners.piedrapapeltijera.ui.theme.PiedraPapelTijeraTheme
 
 /*Rutas*/
@@ -28,30 +36,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PiedraPapelTijeraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppRoot()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppRoot() {
+    PiedraPapelTijeraTheme {
+        val nav = rememberNavController()
+        MaterialTheme {
+            NavHost(
+                navController = nav,
+                startDestination = Screen.Home.route
+            ){
+                composable(Screen.Home.route) { HomeScreen()}
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PiedraPapelTijeraTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Piedra, Papel o Tijera", style = MaterialTheme.typography.titleLarge)
     }
 }

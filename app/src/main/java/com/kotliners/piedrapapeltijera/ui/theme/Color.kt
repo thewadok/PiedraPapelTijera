0e386cbded75f1ad6d592b3666a775ba4552ba07
thewadok <erodriguezrodriguez5@uoc.edu>
2025-10-21 21:13:29 +0200

    feat: Creamos paleta de colores de la aplicación en Color.kt

--- app/src/main/java/com/kotliners/piedrapapeltijera/ui/theme/Color.kt
+++ app/src/main/java/com/kotliners/piedrapapeltijera/ui/theme/Color.kt
@@ -9,3 +9,23 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Paleta de colores para Kotliners
+
+//Color general para el fondo de pantallas
+val FondoNegro = Color(0xFF000000)
+
+//Color para el texto e iconos sobre fondo negro u oscuros
+val TextoBlanco = Color(0xFFFFFFFF)
+
+//Color para el texto e iconos sobre fondo blanco o claros
+val TextoNegro = Color(0xFF000000)
+
+//Colores neon para botones, titulos, indicadores, detalles a resaltar
+val RosaNeon = Color(0xFFFF1493)
+val AmarilloNeon = Color(0xFFFFEA00)
+val AzulNeon = Color(0xFF00E5FF)
+
+//Color GrisClaro para elementos no activos
+val GrisClaro = Color(0xFFB0B0B0)
+

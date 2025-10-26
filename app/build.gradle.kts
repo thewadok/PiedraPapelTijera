e82bd56593ce1e03f39fc25c23b5fe9b7b0dbb75
Yani Giatas <jgiatas@uoc.edu>
2025-10-26 19:50:13 +0100

    Agregando librerías y versión room para SQLite al gradle de la app y del proyecto y actualizando librería room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
+val room_version = "2.6.1"
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp") // Agregando KSP
 }
 
 android {
@@ -54,6 +57,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.6")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.room:room-runtime:$room_version") //Librería SQLite
+    implementation("androidx.room:room-ktx:${room_version}") //Para Corutinas en SQLite
+    ksp("androidx.room:room-compiler:${room_version}")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

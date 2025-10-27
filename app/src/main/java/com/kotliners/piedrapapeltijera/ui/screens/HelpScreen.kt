6802d0dc21442c62b5bec60a59c8f50a08447616
thewadok <erodriguezrodriguez5@uoc.edu>
2025-10-27 22:40:56 +0100

    refactor: Separamos la lógica principal y componentes en paquetes ui y ui.components

---
+++ app/src/main/java/com/kotliners/piedrapapeltijera/ui/screens/HelpScreen.kt
@@ -0,0 +1,8 @@
+package com.kotliners.piedrapapeltijera.ui.screens
+
+import androidx.compose.runtime.Composable
+import com.kotliners.piedrapapeltijera.ui.components.Center
+
+//Pantalla Ayuda
+@Composable
+fun HelpScreen() = Center ( "Ayuda y Reglas")

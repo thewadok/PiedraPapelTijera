22200e4b3b1e716214c982280a4ca4c4e3f6c60a
thewadok <erodriguezrodriguez5@uoc.edu>
2025-10-28 17:02:39 +0100

    Update GameScreen.kt

--- app/src/main/java/com/kotliners/piedrapapeltijera/ui/screens/GameScreen.kt
+++ app/src/main/java/com/kotliners/piedrapapeltijera/ui/screens/GameScreen.kt
@@ -1,8 +1,20 @@
 package com.kotliners.piedrapapeltijera.ui.screens
 
-import androidx.compose.runtime.Composable
-import com.kotliners.piedrapapeltijera.ui.components.Center
+import androidx.compose.runtime.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.kotliners.piedrapapeltijera.R
+import com.kotliners.piedrapapeltijera.game.GameLogic
+import com.kotliners.piedrapapeltijera.game.PlayerState
+import com.kotliners.piedrapapeltijera.ui.theme.FondoNegro
 
-//Pantalla Juego
-@Composable
-fun GameScreen() = Center ( "Juego")

88660a18dcf591826f4bd3665f57140e469df63f
thewadok <erodriguezrodriguez5@uoc.edu>
2025-10-30 18:00:58 +0100

    feat: Implementamos PartidaDao para guardar y consultar partidas

---
+++ app/src/main/java/com/kotliners/piedrapapeltijera/data/local/dao/PartidaDao.kt
@@ -0,0 +1,25 @@
+package com.kotliners.piedrapapeltijera.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.kotliners.piedrapapeltijera.data.local.entity.Partida
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Single
+
+@Dao
+interface PartidaDao {
+
+    @Insert
+    fun insertar(partida: Partida): Completable
+
+    @Query("SELECT * FROM partidas ORDER BY fecha DESC")
+    fun observarHistorial(): Flowable<List<Partida>>
+
+    @Query("SELECT COUNT(*) FROM partidas")
+    fun contar(): Single<Int>
+
+    @Query("DELETE FROM partidas")
+    fun borrarTodo(): Completable
+}

14ac21c421791b580bc3b7473b1eeb1573bd36b1
thewadok <erodriguezrodriguez5@uoc.edu>
2025-11-09 17:58:08 +0100

    feat: Creamos la clase VictoryManager para centralizar los eventos de guarda captura y guardar en calendario

---
+++ app/src/main/java/com/kotliners/piedrapapeltijera/utils/victory/VictoryManager.kt
@@ -0,0 +1,22 @@
+package com.kotliners.piedrapapeltijera.utils.victory
+
+import android.content.Context
+import android.graphics.Bitmap
+import com.kotliners.piedrapapeltijera.utils.media.ScreenshotSaver
+import com.kotliners.piedrapapeltijera.utils.calendar.CalendarHelper
+
+/* Clase para la gestión de los eventos que desencadena ganar una partida,
+y centralizar la lógica en una función.
+ */
+class VictoryManager {
+
+    // Función suspend para gestionar la victoria sin bloquear la interfaz
+    suspend fun handleVictory(context: Context, bitmap: Bitmap) {
+
+        // Guardamos la captura de pantalla
+        ScreenshotSaver.saveToMediaStore(context, bitmap)
+
+        // Añadimos el evento al calendario
+        CalendarHelper.insertVictoryEvent(context)
+    }
+}

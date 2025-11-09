d3e828f71c0f96525e9cb3d5aa01ad5139580650
thewadok <erodriguezrodriguez5@uoc.edu>
2025-11-09 12:52:54 +0100

    feat: Solicitamos permisos de calendario en MainActivity

--- app/src/main/java/com/kotliners/piedrapapeltijera/MainActivity.kt
+++ app/src/main/java/com/kotliners/piedrapapeltijera/MainActivity.kt
@@ -9,14 +9,33 @@ import androidx.activity.viewModels
 import com.kotliners.piedrapapeltijera.ui.theme.*
 import com.kotliners.piedrapapeltijera.ui.AppRoot
 import com.kotliners.piedrapapeltijera.ui.viewmodel.MainViewModel
+import androidx.activity.result.contract.ActivityResultContracts
 
 //Activity principal
 class MainActivity : ComponentActivity() {
 
     private val viewModel: MainViewModel by viewModels()
 
+    // Pedimos permisos de calendario
+    private val requestCalendarPerms = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { perms ->
+        val granted = perms[android.Manifest.permission.WRITE_CALENDAR] == true &&
+                perms[android.Manifest.permission.READ_CALENDAR] == true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Solicitamos permisos al iniciar
+        requestCalendarPerms.launch(
+            arrayOf(
+                android.Manifest.permission.READ_CALENDAR,
+                android.Manifest.permission.WRITE_CALENDAR
+            )
+        )
+
+
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(FondoNegro.value.toInt()),
             navigationBarStyle = SystemBarStyle.dark(FondoNegro.value.toInt())

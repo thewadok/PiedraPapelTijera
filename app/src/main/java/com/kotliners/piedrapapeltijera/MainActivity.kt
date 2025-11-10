48b7feefc302ed7f4b941b64563e1129b39f8e27
Jose <josediaz.quijano@gmail.com>
2025-11-10 19:37:10 +0100

    actualizo MainActivity para solicitar permiso de notificaciones y strings.xml para añadir textos de notificacion

--- app/src/main/java/com/kotliners/piedrapapeltijera/MainActivity.kt
+++ app/src/main/java/com/kotliners/piedrapapeltijera/MainActivity.kt
@@ -9,20 +9,26 @@ import androidx.activity.viewModels
 import com.kotliners.piedrapapeltijera.ui.theme.*
 import com.kotliners.piedrapapeltijera.ui.AppRoot
 import com.kotliners.piedrapapeltijera.ui.viewmodel.MainViewModel
+import com.kotliners.piedrapapeltijera.utils.NotificationsPermission
 
-//Activity principal
+// Activity principal
 class MainActivity : ComponentActivity() {
 
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(FondoNegro.value.toInt()),
             navigationBarStyle = SystemBarStyle.dark(FondoNegro.value.toInt())
         )
+
         setContent {
             AppRoot()
         }
+
+        // ✅ Solicita permiso de notificaciones (solo Android 13+)
+        NotificationsPermission.requestIfNeeded(this)
     }
 }

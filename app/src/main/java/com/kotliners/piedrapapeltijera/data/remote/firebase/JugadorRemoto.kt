26bc7fb115a0e26e5a61f40b018cd2c48a39b9c2
thewadok <erodriguezrodriguez5@uoc.edu>
2025-12-09 20:13:12 +0100

    feat: Creamos modelo JugadorRemoto para Firebase

---
+++ app/src/main/java/com/kotliners/piedrapapeltijera/data/remote/firebase/JugadorRemoto.kt
@@ -0,0 +1,10 @@
+package com.kotliners.piedrapapeltijera.data.remote.firebase
+
+// Modelo del jugador en Firebase
+data class JugadorRemoto(
+    val uid: String? = null,
+    val nombre: String? = null,
+    val monedas: Int? = null,
+    val victorias: Int? = null,
+    val derrotas: Int? = null
+)
